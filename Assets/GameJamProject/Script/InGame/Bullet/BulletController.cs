df76bbce35c5a8f17dffb4dd3a7439cfbcd9f5a2
GotouTamaki <134473633+GotouTamaki@users.noreply.github.com>
2024-08-06 12:31:02 +0900

    [add]BulletController作成

---
+++ Assets/GameJamProject/Script/InGame/Bullet/BulletController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    [SerializeField] private string _name;
+    [SerializeField] private Sprite _sprite;
+    [SerializeField] private int _damage;
+    [SerializeField] private float _bulletSpeed;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
