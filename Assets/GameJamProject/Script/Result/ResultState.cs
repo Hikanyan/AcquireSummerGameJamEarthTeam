df6a3caedfcafd6b923d221d726548645faf1ad5
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-08-07 15:07:17 +0900

    [Update] Audioの登録

--- Assets/GameJamProject/Script/Result/ResultState.cs
+++ Assets/GameJamProject/Script/Result/ResultState.cs
@@ -1,4 +1,5 @@
-﻿using GameJamProject.System;
+﻿using GameJamProject.Audio;
+using GameJamProject.System;
 using UnityEngine;
 using NotImplementedException = System.NotImplementedException;
 
@@ -6,16 +7,19 @@ namespace GameJamProject.Script.Result
 {
     public class ResultState : State
     {
-        public override void OnEnter()
+        public override async void OnEnter()
         {
             Debug.Log("Entered Result State");
             // 結果シーンの初期化処理をここに追加
+            await SceneManagement.SceneManager.Instance.LoadScene("ResultScene");
+            AudioManager.Instance.PlayBGM("configClear");
         }
 
         public override void OnExit()
         {
             Debug.Log("Exited Result State");
             // 結果シーンの終了処理をここに追加
+            AudioManager.Instance.StopBGM();
         }
 
         public override void OnUpdate(float deltaTime)
