cd56683707b6232c3dc2655851a5a0029536f986
KoiiroHikanyan <hikanyan0131@gmail.com>
2024-08-07 16:57:20 +0900

    [Updat]

--- Assets/GameJamProject/Script/InGame/Player/PlyerController.cs
+++ Assets/GameJamProject/Script/InGame/Player/PlyerController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlyerController : PlayerParameter
+public class PlyerController : MonoBehaviour
 {
     [SerializeField] float _playerSpeed = 10.0F;
     [SerializeField] Vector2 _min = new Vector2();
     [SerializeField] Vector2 _max = new Vector2();
     [SerializeField] List<GameObject> _items;
+
+    [SerializeField] PlayerHp _playerHp;
+
     private Vector2 _inputMove;
 
     private Transform _transform;
@@ -62,7 +65,7 @@ public class PlyerController : PlayerParameter
     /// <param name="context"></param>
     public void OnFire(InputAction.CallbackContext context)
     {
-        if(context.performed)
+        if (context.performed)
         {
             SelectItem(_items[Random.Range(0, _items.Count)]);
             AudioManager.Instance.PlaySE("‘å–C2");
